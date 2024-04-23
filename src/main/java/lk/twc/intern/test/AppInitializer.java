b5ffd92e8ce9bcc50d23f6cb90b6fe460af22baf
Pravinda-Bandara <bandarapravinda@gmail.com>
2024-04-24 01:51:59 +0530

    add model mapper as bean

--- src/main/java/lk/twc/intern/test/AppInitializer.java
+++ src/main/java/lk/twc/intern/test/AppInitializer.java
@@ -1,10 +1,16 @@
 package lk.twc.intern.test;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppInitializer {
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AppInitializer.class, args);
